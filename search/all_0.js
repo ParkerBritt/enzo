240e9f34cda813f5dbfd8641ea93ab9ea7a71052
ParkerBritt <ParkerBritt@users.noreply.github.com>
2025-08-11 00:48:36 +0000

    deploy: 6da03e2e234cc29c0082a38ea3c5d0e7776f06c0

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['attribute_0',['Attribute',['../classenzo_1_1ga_1_1Attribute.html',1,'enzo::ga']]],
-  ['attributehandle_1',['AttributeHandle',['../classenzo_1_1ga_1_1AttributeHandle.html',1,'enzo::ga']]]
+  ['attributehandle_1',['AttributeHandle',['../classenzo_1_1ga_1_1AttributeHandle.html',1,'enzo::ga']]],
+  ['attributehandlero_2',['AttributeHandleRO',['../classenzo_1_1ga_1_1AttributeHandleRO.html',1,'enzo::ga']]]
 ];
