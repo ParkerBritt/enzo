77e982d37fece76bf470770ee00ffd273af1be94
parker <parker.g.britt@gmail.com>
2026-04-06 19:54:05 +0100

    feat: add batched undo to int slider, and combine duplicate code in slider parms

--- src/Gui/Parameters/FloatSliderParm.h
+++ src/Gui/Parameters/FloatSliderParm.h
@@ -1,44 +1,23 @@
 #pragma once
-#include "Engine/Parameter/Parameter.h"
 #include "Engine/Types.h"
-#include "Engine/UndoRedo/UndoDisabler.h"
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QWidget>
-#include <boost/signals2/connection.hpp>
-#include <memory>
-#include <optional>
+#include "Gui/Parameters/SliderParmBase.h"
 
 namespace enzo::ui {
 
-class FloatSliderParm : public QWidget {
+class FloatSliderParm : public SliderParmBase {
     Q_OBJECT
   public:
     FloatSliderParm(std::weak_ptr<prm::Parameter> parameter, unsigned int vectorIndex = 0,
                     QWidget *parent = nullptr, Qt::WindowFlags f = Qt::WindowFlags());
 
-  private:
-    unsigned int vectorIndex_; // index into the parameter's value vector, e.g. 0=X, 1=Y, 2=Z for XYZ types
-    QVBoxLayout *mainLayout_;
-    QLabel *valueLabel_;
-    bt::floatT value_;
-    bool clampMin_;
-    bool clampMax_;
-    bt::floatT minValue_;
-    bt::floatT maxValue_;
-    std::weak_ptr<prm::Parameter> parameter_;
-
-    void setValueImpl(bt::floatT value);
-    boost::signals2::scoped_connection valueChangedConnection_;
-
-    std::optional<UndoDisabler> undoDisabler_;
-    prm::PrmValues valueBeforeDrag_;
-
   protected:
     void paintEvent(QPaintEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
+    void syncFromParameter() override;
+    void applyValue(float normalizedValue) override;
+
+  private:
+    bt::floatT value_;
+    void setValueImpl(bt::floatT value);
 };
 
 } // namespace enzo::ui
--- src/Gui/Parameters/IntSliderParm.h
+++ src/Gui/Parameters/IntSliderParm.h
@@ -1,42 +1,26 @@
 #pragma once
-#include "Engine/Parameter/Parameter.h"
 #include "Engine/Types.h"
-#include <QLabel>
+#include "Gui/Parameters/SliderParmBase.h"
 #include <QPen>
-#include <QVBoxLayout>
-#include <QWidget>
-#include <boost/signals2/connection.hpp>
 
 namespace enzo::ui {
 
-class IntSliderParm : public QWidget {
+class IntSliderParm : public SliderParmBase {
     Q_OBJECT
   public:
     IntSliderParm(std::weak_ptr<enzo::prm::Parameter> parameter, QWidget *parent = nullptr,
                   Qt::WindowFlags f = Qt::WindowFlags());
 
-  private:
-    QVBoxLayout *mainLayout_;
-    QLabel *valueLabel_;
-    bt::intT value_;
-    bool clampMin_;
-    bool clampMax_;
-    bt::intT minValue_;
-    bt::intT maxValue_;
-
-    std::weak_ptr<prm::Parameter> parameter_;
-
-    QPen notchPen_;
-    static constexpr int notchWidth = 2;
-
-    void setValueImpl(bt::intT value);
-    boost::signals2::scoped_connection valueChangedConnection_;
-
   protected:
     void paintEvent(QPaintEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
+    void syncFromParameter() override;
+    void applyValue(float normalizedValue) override;
+
+  private:
+    bt::intT value_;
+    QPen notchPen_;
+    static constexpr int notchWidth = 2;
+    void setValueImpl(bt::intT value);
 };
 
 } // namespace enzo::ui
-
