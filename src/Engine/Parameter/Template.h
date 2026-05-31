208f3edee997ca1adffe5228b0387fa5a6e7e099
parker <parker.g.britt@gmail.com>
2026-05-31 23:12:26 +0100

    feat: disable parm background for xyz by default

--- src/Engine/Parameter/Template.h
+++ src/Engine/Parameter/Template.h
@@ -9,47 +9,29 @@
 #include <utility>
 #include <vector>
 
-namespace enzo::prm
-{
+namespace enzo::prm {
 
-class Template
-{
-public:
-    Template(
-        enzo::prm::Type type,
-        prm::Name name
-    );
-    Template(
-        enzo::prm::Type type,
-        prm::Name name,
-        unsigned int vectorSize
-    );
-    Template(
-        enzo::prm::Type type,
-        prm::Name name,
-        std::vector<prm::Default> defaults,
-        unsigned int vectorSize = 1,
-        std::vector<prm::Range> ranges=std::vector<prm::Range>()
-    );
-    Template(
-        enzo::prm::Type type,
-        prm::Name name,
-        prm::Default theDefault,
-        unsigned int vectorSize = 1,
-        Range range=Range()
-    );
+class Template {
+  public:
+    Template(enzo::prm::Type type, prm::Name name);
+    Template(enzo::prm::Type type, prm::Name name, unsigned int vectorSize);
+    Template(enzo::prm::Type type, prm::Name name, std::vector<prm::Default> defaults,
+             unsigned int vectorSize = 1,
+             std::vector<prm::Range> ranges = std::vector<prm::Range>());
+    Template(enzo::prm::Type type, prm::Name name, prm::Default theDefault,
+             unsigned int vectorSize = 1, Range range = Range());
     // get name and get token are identical
     enzo::bt::String getName() const;
     enzo::bt::String getToken() const;
     enzo::bt::String getLabel() const;
-    const prm::Default getDefault(unsigned int index=0) const;
-    const prm::Range& getRange(unsigned int index=0) const;
+    const prm::Default getDefault(unsigned int index = 0) const;
+    const prm::Range &getRange(unsigned int index = 0) const;
     const prm::Type getType() const;
     const unsigned int getSize() const;
     const unsigned int getNumDefaults() const;
 
     Direction getDirection() const;
-    const std::vector<Template>& getChildren() const;
+    const std::vector<Template> &getChildren() const;
     const bool isContainer() const;
 
     enzo::bt::String getTooltip() const;
@@ -59,26 +41,24 @@ public:
 
     // Chainable setters. Mutate in place and return *this so a Template can
     // be configured inline at construction.
-    Template& setTooltip(bt::String tooltip);
-    Template& setDocumentation(bt::String documentation);
-    Template& setDirection(Direction direction);
-    Template& addParm(Template child);
-    Template& setLabelHidden(bool hidden);
-    Template& setBackgroundEnabled(bool enabled);
+    Template &setTooltip(bt::String tooltip);
+    Template &setDocumentation(bt::String documentation);
+    Template &setDirection(Direction direction);
+    Template &addParm(Template child);
+    Template &setLabelHidden(bool hidden);
+    Template &setBackgroundEnabled(bool enabled);
 
     // Style structs may hold parameters with change signals, which cannot
     // be copied. Wrapping in a shared pointer lets the style live inside
     // the std::any without ever being copied.
-    template <typename T>
-    Template& setStyle(T style)
-    {
+    template <typename T> Template &setStyle(T style) {
         style_ = std::make_shared<T>(std::move(style));
         return *this;
     }
 
-    const std::any& getStyle() const;
+    const std::any &getStyle() const;
 
-private:
+  private:
     enzo::prm::Type type_;
     std::vector<prm::Default> defaults_;
     std::vector<prm::Range> ranges_;
@@ -95,7 +75,8 @@ private:
     std::vector<Template> children_;
     std::any style_;
     inline const static std::unordered_set<prm::Type> containerTypes_ = {prm::Type::GROUP};
-    inline const static std::unordered_set<prm::Type> backgroundDisabledByDefault_ = {prm::Type::GROUP};
+    inline const static std::unordered_set<prm::Type> backgroundDisabledByDefault_ = {
+        prm::Type::GROUP, prm::Type::XYZ};
 };
 
-}
+} // namespace enzo::prm
