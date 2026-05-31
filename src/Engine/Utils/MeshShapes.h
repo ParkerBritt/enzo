c199c9510461e28961f97b67b3c30d29508090f5
parker <parker.g.britt@gmail.com>
2026-05-31 14:51:28 +0100

    feat: detriangulate boolean

---
+++ src/Engine/Utils/MeshShapes.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Engine/Types.h"
+#include <memory>
+
+namespace enzo::geo { class Mesh; }
+
+namespace enzo::utils {
+
+/**
+ * @brief Builds an axis aligned cube as a Mesh with 8 points and 6 quad faces.
+ *
+ * Face windings follow the right hand rule on CCW order viewed from outside.
+ * @param size Extent of the cube along each axis.
+ * @param center World position of the cube's center.
+ * @return Shared pointer to the new cube mesh.
+ */
+std::shared_ptr<geo::Mesh> buildCube(const bt::Vector3& size, const bt::Vector3& center);
+
+} // namespace enzo::utils
