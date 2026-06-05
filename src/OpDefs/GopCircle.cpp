4573aff116a996b9e3f9850bfee51a57fe0f62b0
parker <parker.g.britt@gmail.com>
2026-06-05 20:11:34 +0100

    feat(circle node): add divisions and uniform scale parameters

--- src/OpDefs/GopCircle.cpp
+++ src/OpDefs/GopCircle.cpp
@@ -6,6 +6,7 @@
 #include <Eigen/Geometry>
 #include <memory>
 #include <numbers>
+#include <vector>
 
 GopCircle::GopCircle(enzo::nt::NetworkManager* network, enzo::op::OpInfo opInfo)
     : GeometryOpDef(network, opInfo)
@@ -18,13 +19,20 @@ void GopCircle::cookOp(enzo::op::Context context)
 
     NodePacket packet;
     std::shared_ptr<geo::Mesh> mesh = std::make_shared<geo::Mesh>();
-    
-    int numPoints = 10;
-    for(int i=0; i<numPoints; ++i)
+
+    int numPoints = context.evalIntParm("divisions");
+    float uniform_scale = context.evalFloatParm("uniform_scale");
+
+    std::vector<enzo::Vector3> newPointPositions;
+    newPointPositions.reserve(numPoints);
+
+    for (int i = 0; i < numPoints; ++i)
     {
-        float delta = static_cast<float>(i)/numPoints*std::numbers::pi*2;
-        mesh->addPoint({sin(delta),0,cos(delta)});
+        float delta = static_cast<float>(i) / numPoints * std::numbers::pi * 2;
+        newPointPositions.push_back({sin(delta) * uniform_scale, 0, cos(delta) * uniform_scale});
     }
+    auto newPoints = mesh->addPoints(newPointPositions);
+    mesh->addFace(newPoints);
 
     packet.addPrimitive(std::move(mesh));
     setOutputPacket(0, packet);
@@ -34,6 +42,20 @@ std::vector<enzo::prm::Template> GopCircle::parameterList()
 {
     using namespace enzo::prm;
     return {
+        enzo::prm::Template(
+            enzo::prm::Type::FLOAT,
+            enzo::prm::Name("uniform_scale", "Uniform Scale"),
+            enzo::prm::Default(1),
+            1,
+            enzo::prm::Range(0, 10, enzo::prm::RangeFlag::UNLOCKED, enzo::prm::RangeFlag::UNLOCKED)
+        ),
+        enzo::prm::Template(
+            enzo::prm::Type::INT,
+            enzo::prm::Name("divisions", "Divisions"),
+            enzo::prm::Default(10),
+            1,
+            enzo::prm::Range(1, 100, enzo::prm::RangeFlag::LOCKED, enzo::prm::RangeFlag::UNLOCKED)
+        ),
 
     };
 }
