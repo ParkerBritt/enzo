af91b8c7bc3ec2da8af1a571a61ce6aa9bdbc9c7
ParkerBritt <ParkerBritt@users.noreply.github.com>
2026-06-29 20:08:14 +0000

    deploy: d862c9fa2f9557ec7bdc0be96d8a76c8dbe5935f

--- search/all_15.js
+++ search/all_15.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['wildcardindexset_0',['WildcardIndexSet',['../classenzo_1_1WildcardIndexSet.html',1,'enzo']]]
+  ['wildcardindexset_0',['WildcardIndexSet',['../classenzo_1_1WildcardIndexSet.html',1,'enzo']]],
+  ['with_20daslang_1',['installed with daslang',['../index.html#autotoc_md3',1,'']]]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -1,16 +1,17 @@
 var searchData=
 [
   ['dascontext_0',['DasContext',['../structenzo_1_1expr_1_1DasContext.html',1,'enzo::expr']]],
-  ['dasruntime_1',['DasRuntime',['../classenzo_1_1expr_1_1DasRuntime.html',1,'enzo::expr']]],
-  ['default_2',['Default',['../classenzo_1_1prm_1_1Default.html',1,'enzo::prm']]],
-  ['defragment_3',['defragment',['../classenzo_1_1geo_1_1Mesh.html#af039b0c050656f9e7ea14339ac27d0cd',1,'enzo::geo::Mesh::defragment()'],['../classenzo_1_1geo_1_1Primitive.html#af26e93804c110b1333b0dfa13a3e1ed2',1,'enzo::geo::Primitive::defragment()']]],
-  ['deleteallfaces_4',['deleteAllFaces',['../classenzo_1_1geo_1_1Mesh.html#a159cbe87411578c684da77c2133e4a81',1,'enzo::geo::Mesh']]],
-  ['deletenode_5',['deleteNode',['../classenzo_1_1nt_1_1NetworkManager.html#aa0921669d2f73b4563ec2b127c943a2b',1,'enzo::nt::NetworkManager']]],
-  ['deletenodecommand_6',['DeleteNodeCommand',['../classenzo_1_1nt_1_1DeleteNodeCommand.html',1,'enzo::nt']]],
-  ['dependencies_7',['Dependencies',['../index.html#autotoc_md2',1,'']]],
-  ['dirtynode_8',['dirtyNode',['../classenzo_1_1nt_1_1GeometryOperator.html#a892fe23575ba1bf13956f3cb414d0922',1,'enzo::nt::GeometryOperator']]],
-  ['disconnect_9',['disconnect',['../classenzo_1_1nt_1_1NetworkGraph.html#a555a11033ea0bd0707594eff2e85285f',1,'enzo::nt::NetworkGraph']]],
-  ['disconnectnodes_10',['disconnectNodes',['../classenzo_1_1nt_1_1NetworkManager.html#a9ee0120a53b7e3dec885444904c0ecb9',1,'enzo::nt::NetworkManager']]],
-  ['documentation_11',['Documentation',['../index.html#autotoc_md3',1,'']]],
-  ['duplicatepoints_12',['duplicatePoints',['../classenzo_1_1geo_1_1Mesh.html#a15b42a593ccf24a9dcd51bcc228bb066',1,'enzo::geo::Mesh']]]
+  ['daslang_1',['installed with daslang',['../index.html#autotoc_md3',1,'']]],
+  ['dasruntime_2',['DasRuntime',['../classenzo_1_1expr_1_1DasRuntime.html',1,'enzo::expr']]],
+  ['default_3',['Default',['../classenzo_1_1prm_1_1Default.html',1,'enzo::prm']]],
+  ['defragment_4',['defragment',['../classenzo_1_1geo_1_1Mesh.html#af039b0c050656f9e7ea14339ac27d0cd',1,'enzo::geo::Mesh::defragment()'],['../classenzo_1_1geo_1_1Primitive.html#af26e93804c110b1333b0dfa13a3e1ed2',1,'enzo::geo::Primitive::defragment()']]],
+  ['deleteallfaces_5',['deleteAllFaces',['../classenzo_1_1geo_1_1Mesh.html#a159cbe87411578c684da77c2133e4a81',1,'enzo::geo::Mesh']]],
+  ['deletenode_6',['deleteNode',['../classenzo_1_1nt_1_1NetworkManager.html#aa0921669d2f73b4563ec2b127c943a2b',1,'enzo::nt::NetworkManager']]],
+  ['deletenodecommand_7',['DeleteNodeCommand',['../classenzo_1_1nt_1_1DeleteNodeCommand.html',1,'enzo::nt']]],
+  ['dependencies_8',['Dependencies',['../index.html#autotoc_md2',1,'']]],
+  ['dirtynode_9',['dirtyNode',['../classenzo_1_1nt_1_1GeometryOperator.html#a892fe23575ba1bf13956f3cb414d0922',1,'enzo::nt::GeometryOperator']]],
+  ['disconnect_10',['disconnect',['../classenzo_1_1nt_1_1NetworkGraph.html#a555a11033ea0bd0707594eff2e85285f',1,'enzo::nt::NetworkGraph']]],
+  ['disconnectnodes_11',['disconnectNodes',['../classenzo_1_1nt_1_1NetworkManager.html#a9ee0120a53b7e3dec885444904c0ecb9',1,'enzo::nt::NetworkManager']]],
+  ['documentation_12',['Documentation',['../index.html#autotoc_md4',1,'']]],
+  ['duplicatepoints_13',['duplicatePoints',['../classenzo_1_1geo_1_1Mesh.html#a15b42a593ccf24a9dcd51bcc228bb066',1,'enzo::geo::Mesh']]]
 ];
