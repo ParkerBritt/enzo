5fc9e96c0a5098aeef845f402b56b1709774895c
parker <parker.g.britt@gmail.com>
2026-09-14 02:53:23 +0100

    feat: add nsis installer

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -1,10 +1,11 @@
-# Turns the installed tree into RPM, DEB, tar.gz, MSI and ZIP packages, and
-# gathers the Qt and third party libraries the application loads at runtime.
+# Turns the installed tree into RPM, DEB, tar.gz, MSI, EXE installer and ZIP
+# packages, and gathers the Qt and third party libraries the application loads
+# at runtime.
 
 set(CPACK_PROJECT_CONFIG_FILE ${CMAKE_SOURCE_DIR}/cmake/CPackProjectConfig.cmake)
 
 if(WIN32)
-    set(CPACK_GENERATOR "WIX;ZIP")
+    set(CPACK_GENERATOR "WIX;NSIS;ZIP")
 else()
     set(CPACK_GENERATOR "RPM;DEB;TGZ")
 endif()
@@ -29,6 +30,11 @@ else()
     set(CPACK_SYSTEM_NAME linux-${CMAKE_SYSTEM_PROCESSOR})
 endif()
 
+# Offers to run the launcher when the installer finishes, and shows its icon in
+# the installed programs list.
+set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${AppExec}.exe")
+set(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\${AppExec}.exe")
+
 set_target_properties(${AppExec} PROPERTIES
     INSTALL_RPATH "$ORIGIN/../lib;$ORIGIN/../../lib"
 )
@@ -47,10 +53,10 @@ qt_generate_deploy_qml_app_script(
 install(CODE "
     set(QT_DEPLOY_BIN_DIR \"${ENZO_BIN_DIR}\")
     set(QT_DEPLOY_LIB_DIR \"${ENZO_LIB_DIR}\")
-    set(QT_DEPLOY_LIBEXEC_DIR \"${CMAKE_PROJECT_NAME}/libexec\")
-    set(QT_DEPLOY_PLUGINS_DIR \"${CMAKE_PROJECT_NAME}/plugins\")
-    set(QT_DEPLOY_QML_DIR \"${CMAKE_PROJECT_NAME}/qml\")
-    set(QT_DEPLOY_TRANSLATIONS_DIR \"${CMAKE_PROJECT_NAME}/translations\")
+    set(QT_DEPLOY_LIBEXEC_DIR \"${ENZO_APP_DIR}libexec\")
+    set(QT_DEPLOY_PLUGINS_DIR \"${ENZO_APP_DIR}plugins\")
+    set(QT_DEPLOY_QML_DIR \"${ENZO_APP_DIR}qml\")
+    set(QT_DEPLOY_TRANSLATIONS_DIR \"${ENZO_APP_DIR}translations\")
 ")
 install(SCRIPT ${ENZO_QT_DEPLOY_SCRIPT})
 
@@ -93,28 +99,30 @@ if(UNIX)
     ")
 endif()
 
-# The desktop entry, metadata and icon Linux desktops read.
-install(FILES flatpak/org.enzosoftware.Enzo.desktop
-    DESTINATION share/applications
-)
-install(FILES flatpak/org.enzosoftware.Enzo.metainfo.xml
-    DESTINATION share/metainfo
-)
-install(FILES static/icons/icon-main-white-square.svg
-    DESTINATION share/icons/hicolor/scalable/apps
-    RENAME org.enzosoftware.Enzo.svg
-)
+# Installs the desktop entry, metadata and icon Linux desktops read.
+if(UNIX)
+    install(FILES flatpak/org.enzosoftware.Enzo.desktop
+        DESTINATION share/applications
+    )
+    install(FILES flatpak/org.enzosoftware.Enzo.metainfo.xml
+        DESTINATION share/metainfo
+    )
+    install(FILES static/icons/icon-main-white-square.svg
+        DESTINATION share/icons/hicolor/scalable/apps
+        RENAME org.enzosoftware.Enzo.svg
+    )
+endif()
 
 # The fonts and icons the application reads at runtime. The theme is compiled
 # into the binary.
 install(DIRECTORY static/
-    DESTINATION ${CMAKE_PROJECT_NAME}/share
+    DESTINATION ${ENZO_APP_DIR}share
     PATTERN "theme" EXCLUDE
 )
 
 # Installs daslib beside enzo/bin, where the runtime looks for the standard
 # library.
-install(DIRECTORY ${DAS_DASLIB_DIR}
-    DESTINATION ${CMAKE_PROJECT_NAME}
+install(DIRECTORY ${DAS_DASLIB_DIR}/
+    DESTINATION ${ENZO_APP_DIR}daslib
 )
 
